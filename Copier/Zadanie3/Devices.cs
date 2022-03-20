c8f97b7c91710f2ce975dc3d9f53a7d4db7bcc1f
lgancarczyk <lukgan2000@gmail.com>
2022-03-20 21:14:20 +0100

    poczatek zadanie 3

---
+++ Copier/Zadanie3/Devices.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie3
+{
+    public interface IDevice
+    {
+        enum State { on, off };
+
+        void PowerOn(); // uruchamia urządzenie, zmienia stan na `on`
+        void PowerOff(); // wyłącza urządzenie, zmienia stan na `off
+        State GetState(); // zwraca aktualny stan urządzenia
+
+        int Counter { get; }  // zwraca liczbę charakteryzującą eksploatację urządzenia,
+                              // np. liczbę uruchomień, liczbę wydrukow, liczbę skanów, ...
+    }
+
+    public abstract class BaseDevice : IDevice
+    {
+        protected IDevice.State state = IDevice.State.off;
+        public IDevice.State GetState() => state;
+
+        public void PowerOff()
+        {
+            state = IDevice.State.off;
+            Console.WriteLine("... Device is off !");
+        }
+
+        public void PowerOn()
+        {
+            state = IDevice.State.on;
+            Console.WriteLine("Device is on ...");
+        }
+
+        public int Counter { get; private set; } = 0;
+    }
+
+    public interface IPrinter : IDevice
+    {
+        /// <summary>
+        /// Dokument jest drukowany, jeśli urządzenie włączone. W przeciwnym przypadku nic się nie wykonuje
+        /// </summary>
+        /// <param name="document">obiekt typu IDocument, różny od `null`</param>
+        void Print(in IDocument document);
+    }
+
+    public interface IScanner : IDevice
+    {
+        // dokument jest skanowany, jeśli urządzenie włączone
+        // w przeciwnym przypadku nic się dzieje
+        void Scan(out IDocument document, IDocument.FormatType formatType);
+    }
+
+    public interface IFax : IDevice 
+    {
+        void Send(in IDocument document);
+    }
+
+    public class Copier : IPrinter, IScanner
+    {
+        public int Counter { get; private set; } = 0;
+        public int ScanCounter { get; private set; } = 0;
+        public int PrintCounter { get; private set; } = 0;
+
+        protected IDevice.State state = IDevice.State.off;
+        public IDevice.State GetState() => state;
+
+        public void PowerOff()
+        {
+            state = IDevice.State.off;
+            Console.WriteLine("... Device is off !");
+        }
+
+        public void PowerOn()
+        {
+            if (state == IDevice.State.off)
+            {
+                state = IDevice.State.on;
+                Counter++;
+            }
+            Console.WriteLine("Device is on ...");
+        }
+
+        public void Print(in IDocument document)
+        {
+            if (state == IDevice.State.on)
+            {
+                PrintCounter++;
+                Console.WriteLine($"{DateTime.Now} Print: {document.GetFileName()}");
+            }
+            
+        }
+
+        public void Scan(out IDocument document, IDocument.FormatType formatType = IDocument.FormatType.PDF)
+        {
+            if (formatType == IDocument.FormatType.PDF)
+            {
+                document = new PDFDocument("aaa.pdf");
+            }
+            else if (formatType == IDocument.FormatType.JPG)
+            {
+                document = new ImageDocument("aaa.jpg");
+            }
+            else
+            {
+                document = new TextDocument("aaa.txt");
+            }
+
+            if (state == IDevice.State.on)
+            {
+                ScanCounter++;
+                Console.WriteLine($"{DateTime.Now} Scan: {document.GetFileName()}");
+            }
+        }
+
+        public void ScanAndPrint()
+        {
+            if (state ==IDevice.State.on)
+            {
+                IDocument doc;
+                Scan(out doc, formatType: IDocument.FormatType.JPG);
+                Print(doc);
+            }
+
+        }
+    }
+    public class MultiFunctionalDevice : IPrinter, IScanner, IFax 
+    {
+        public int Counter { get; private set; } = 0;
+        public int ScanCounter { get; private set; } = 0;
+        public int PrintCounter { get; private set; } = 0;
+
+        protected IDevice.State state = IDevice.State.off;
+        public IDevice.State GetState() => state;
+
+        public void PowerOff()
+        {
+            state = IDevice.State.off;
+            Console.WriteLine("... Device is off !");
+        }
+
+        public void PowerOn()
+        {
+            if (state == IDevice.State.off)
+            {
+                state = IDevice.State.on;
+                Counter++;
+            }
+            Console.WriteLine("Device is on ...");
+        }
+
+        public void Print(in IDocument document)
+        {
+            if (state == IDevice.State.on)
+            {
+                PrintCounter++;
+                Console.WriteLine($"{DateTime.Now} Print: {document.GetFileName()}");
+            }
+
+        }
+
+        public void Scan(out IDocument document, IDocument.FormatType formatType = IDocument.FormatType.PDF)
+        {
+            if (formatType == IDocument.FormatType.PDF)
+            {
+                document = new PDFDocument("aaa.pdf");
+            }
+            else if (formatType == IDocument.FormatType.JPG)
+            {
+                document = new ImageDocument("aaa.jpg");
+            }
+            else
+            {
+                document = new TextDocument("aaa.txt");
+            }
+
+            if (state == IDevice.State.on)
+            {
+                ScanCounter++;
+                Console.WriteLine($"{DateTime.Now} Scan: {document.GetFileName()}");
+            }
+        }
+
+        public void ScanAndPrint()
+        {
+            if (state == IDevice.State.on)
+            {
+                IDocument doc;
+                Scan(out doc, formatType: IDocument.FormatType.JPG);
+                Print(doc);
+            }
+
+        }
+
+        public void Send(in IDocument document)
+        {
+            if (state == IDevice.State.on)
+            {
+                Console.WriteLine($"{DateTime.Now} Send: {document.GetFileName()}");
+            }
+        }
+    }
+}
---
+++ Copier/Zadanie3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Zadanie3
+{
+    class Program
+    {
+        static void Main()
+        {
+            var xerox = new Copier();
+            xerox.PowerOn();
+            IDocument doc1 = new PDFDocument("aaa.pdf");
+            xerox.Print(in doc1);
+
+            IDocument doc2;
+            xerox.Scan(out doc2);
+            //xerox.Scan(out doc2, formatType: IDocument.FormatType.JPG);
+
+            xerox.ScanAndPrint();
+            System.Console.WriteLine(xerox.Counter);
+            System.Console.WriteLine(xerox.PrintCounter);
+            System.Console.WriteLine(xerox.ScanCounter);
+
+            var xerox2 = new MultiFunctionalDevice();
+            xerox2.PowerOn();
+            IDocument doc3 = new PDFDocument("aaa.pdf");
+            xerox2.Send(doc3);
+
+        }
+    }
+}
